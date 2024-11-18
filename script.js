d45e1c52f8d295942f4ea933f7ca018be0f74362
davidetoscano129 <toscano.davidew@gmail.com>
2024-11-18 17:47:32 +0100

    add: Style Bonus

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const listElem = document.getElementById("emails");
+const generateButton = document.getElementById("generate-emails");
 
-// Ciclo for per generare 10 email
-for (let i = 0; i < 10; i++) { 
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((resp) => {
-            const email = resp.data.response; 
-            console.log(email); 
-            listElem.innerHTML += `<li>${email}</li>`; 
-        })
-};
+// Funzione per generare email
+function generateEmails() {
+    // Svuota la lista
+    listElem.innerHTML = ''; 
+
+    // Genera 10 email
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                const email = resp.data.response; // Estrai email
+                listElem.innerHTML += `<li>${email}</li>`; // Aggiungi email alla lista
+            })
+    }
+}
+
+// Event listener per il bottone
+generateButton.addEventListener("click", generateEmails);
+
+// Genera una prima lista di email al caricamento della pagina
+generateEmails();
